437da3d67139b203e844044c5102ff462b8b642c
Ricardo JL Rufino <ricardo@edu3.com.br>
2022-04-17 02:41:53 -0300

    change tols package to tar.gz

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ zip -r websim-avr-1.0.zip websim-avr
 
 cd tools
 cp webuploader-src/target/webuploader-jar-with-dependencies.jar webuploader/webuploader.jar
-zip -r webuploader-1.0.0.zip webuploader
+#zip -r webuploader-1.0.0.zip webuploader
+tar -czvf webuploader-1.0.0.tar.gz webuploader
 
-ls -la *.zip
+ls -la *.gz
 cd ..
 ls -la *.zip
